702994a4632d64f94c4d1d5e7fc65b33cfae4f38
AInsolence <antoniovbak14@gmail.com>
2016-08-17 22:13:29 +0600

    test:ok result: create restaurantmenu.db file/

--- database_setup.py
+++ database_setup.py
@@ -1,10 +1,15 @@
-'''Restaurant menu python file'''
+#!/usr/bin/env python
+# database_setup.py -- Python module for Restaurant Menu project.
+# Module create, read, update and delete information on sqlite databases used for that web site.
+# Use sqlite module DOCUMENTATION ## https://www.sqlite.org/docs.html ##
+# Use sqlalchemy module to work with database. DOCUMENTATION ## http://docs.sqlalchemy.org/en/latest ##
+# master branch
 
 #imports#
 import sys
-from sqlalchemy import Column, ForegnKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm importrelationship
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
@@ -12,7 +17,7 @@ Base = declarative_base()
 class Restaurant():
     '''Class for create objects which will representing restaurants'''
     __tablename__ = 'restaurant' #table info
-    '''Mapper'''
+    #mapper
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     
@@ -20,15 +25,16 @@ class Restaurant():
 class MenuItem():
     '''Class for create objects which will representing restaurants'''
     __tablename__ = 'menu_item' #table info
-    '''Mapper'''
+    #mapper
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
     course = Column(String(250))
     description = Column(String(250))
     prise = Column(String(8))
-    restaurant_id = Column(Integer, ForegnKey('restaurant.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
     
-    print ('Hello!')    
+    
+    
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.create_all(engine)
