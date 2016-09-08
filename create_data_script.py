fbbd746d7b83e1a378046b0cf15b7305cbcd1ca2
= <=>
2016-09-08 13:11:44 +0600

    added info in create_data_script how to change data in DB with SQL Alchemy

--- create_data_script.py
+++ create_data_script.py
@@ -1,4 +1,4 @@
-'''Python script added data in our DB'''
+'''Python script changed data in our DB'''
 
 #Import our databases
 from sqlalchemy import create_engine
@@ -18,17 +18,33 @@ session.commit()
 session.query(Restaurant).all()
 
 #Add first new menu item in our menu of Pizza Palace
-cheesepizza = MenuItem(name = 'Cheese Pizza', course = 'Entree', description = 'Made with all natural ingredients and fresh mozarella.', prise = '8.99',restaurant = myFirstRestaurant)
+cheesepizza = MenuItem(name = 'Cheese Pizza', course = 'Entree', description = 'Made with all natural ingredients and fresh mozarella.', price = '$8.99',restaurant = myFirstRestaurant)
 session.add(cheesepizza)
 session.commit()
 #Check result
 session.query(MenuItem).all()
 
 #Add second menu item in our menu of Pizza Palace
-seafoodpizza = MenuItem(name = 'Seafood Pizza', course = 'Entree', description = 'Made with shrimps, salman and smokee anchovies.', prise = '11.50',restaurant = myFirstRestaurant)
+seafoodpizza = MenuItem(name = 'Seafood Pizza', course = 'Entree', description = 'Made with shrimps, salman and smokee anchovies.', price = '$11.50',restaurant = myFirstRestaurant)
 session.add(seafoodpizza)
 session.commit()
 #Check first object and get it name attribute
 firstResult = session.query(Restaurant).first()
 firstResult.name
 
+'''How to update info in SQL Alchemy'''
+#find entries
+veggieBurgers = session.query(MenuItem).filter_by(name = 'Veggie Burger')
+
+for veggieBurger in veggieBurgers:
+    print veggieBurger.id
+    print veggieBurger.price
+    print veggieBurger.restaurant.name
+    print '\n'
+
+UrbanVeggieBurger = session.query(MenuItem).filter_by(id = 8).one()
+print UrbanVeggieBurger.price
+#Update price for instance
+UrbanVeggieBurger.price = '$2.99'
+session.add(UrbanVeggieBurger) # add to session
+session.commit() # commit updated data
--- database_setup.py
+++ database_setup.py
@@ -30,7 +30,7 @@ class MenuItem():
     id = Column(Integer, primary_key = True)
     course = Column(String(250))
     description = Column(String(250))
-    prise = Column(String(8))
+    price = Column(String(8))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
     
