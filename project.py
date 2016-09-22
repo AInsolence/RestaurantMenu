5bb55fb63b5a42428e50b73593992820ecbfbb44
= <=>
2016-09-22 11:53:44 +0600

    restaurant 1 menu objective

--- project.py
+++ project.py
@@ -1,20 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # project.py -- Python module for Restaurant Menu project.
-# Description here
+# Server on Python based on Flask framework
 # Used flask module DOCUMENTATION ## http://flask.pocoo.org/ ##
 # Used sqlalchemy module to interact with database. DOCUMENTATION ## http://docs.sqlalchemy.org/en/latest ##
 # master branch
 
-#imports
+#import framework
 from flask import Flask
+
+'''Interaction with database'''
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+#Create engine and session
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+DBsession = sessionmaker(bind = engine)
+session = DBsession()
+
+'''Server'''
 
 app = Flask(__name__)
 @app.route('/')
 @app.route('/hello')
 
 def HelloWorld():
-    return 'Hello World!!!'
+    restaurant = session.query(Restaurant).first()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+    output = ''
+    for i in items:
+        output += i.name
+        output += '</br>'
+        output += i.price
+        output += '</br>'
+        output += i.description
+        output += '</br></br>'
+    return output
 
     
 if __name__ = '__main__':
