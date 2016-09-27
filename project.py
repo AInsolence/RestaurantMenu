ad3c3b084a066e0a274770dfeb43a7b177b8fe45
= <=>
2016-09-27 14:42:14 +0600

    edit/delete templates, new menu item link +++

--- project.py
+++ project.py
@@ -39,20 +39,29 @@ def newMenuItem(restaurant_id):
 		session.add(newItem)
 		session.commit()
 		return redirect(url_for(restaurantMenu, restaurant_id = restaurant_id))
-    return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+    else:
+    	return render_template('newmenuitem.html', restaurant_id = restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
+	item = session.query(MenuItem).filter_by(id = menu_id).one()
+	if request.method == 'POST':
+		if request.form['newname']:
+			item.name = request.form['newname']
+			session.add(item)
+			session.commit()
+			return redirect(url_for(restaurantMenu, restaurant_id = restaurant_id))
+    else:
+    	return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = item)
 
 # Task 3: Create a route for deleteMenuItem function here
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
     return "page to delete a menu item. Task 3 complete!"
-   
+
 
 
    
