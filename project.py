58d533f67bb602e725df6c16d3c95cd8224b5458
= <=>
2016-09-27 11:43:46 +0600

    created newmenuitem request, POST method related

--- project.py
+++ project.py
@@ -7,7 +7,7 @@
 # master branch
 
 #import framework
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 '''Interaction with database'''
 from sqlalchemy import create_engine
@@ -25,16 +25,21 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/restaurants/<int:restaurant_id>/')
-def restaurantmenu(restaurant_id):
+def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
     return render_template('menu.html', restaurant = restaurant, items = items)
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/new/')
+@app.route('/restaurants/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+	if request.method == 'POST':
+		newItem = MenuItem(name = request.form['name'], restaurant_id = restaurant_id)
+		session.add(newItem)
+		session.commit()
+		return redirect(url_for(restaurantMenu, restaurant_id = restaurant_id))
+    return render_template('newmenuitem.html', restaurant_id = restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
