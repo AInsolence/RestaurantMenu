069bd67e2ad09c83cb2b236f99530b77f3545785
AInsolence <antoniovbak14@gmail.com>
2016-09-27 22:49:07 +0600

    debugged, tested on server, all functions is work well

--- project.py
+++ project.py
@@ -33,8 +33,7 @@ def restaurantMenu(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/menu/JSON')
 def restaurantMenuJSON(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
-    items = session.query(MenuItem).filter_by(
-        restaurant_id=restaurant_id).all()
+    items = session.query(MenuItem).filter_by(restaurant_id=restaurant_id).all()
     return jsonify(MenuItems=[i.serialize for i in items])
 
 # Task 1: Create route for newMenuItem function here
@@ -42,33 +41,32 @@ def restaurantMenuJSON(restaurant_id):
 @app.route('/restaurants/<int:restaurant_id>/new/', methods = ['GET', 'POST'])
 def newMenuItem(restaurant_id):
 	if request.method == 'POST':
-		newItem = MenuItem(name=request.form['name'], description=request.form[
-                           'description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
+		newItem = MenuItem(name=request.form['name'], description=request.form['description'], price=request.form['price'], course=request.form['course'], restaurant_id=restaurant_id)
 		session.add(newItem)
 		session.commit()
 		flash("New item successfully created!")
-		return redirect(url_for(restaurantMenu, restaurant_id = restaurant_id))
-    else:
-    	return render_template('newmenuitem.html', restaurant_id = restaurant_id)
+		return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+	else:
+		return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
 # Task 2: Create route for editMenuItem function here
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods = ['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-	itemToEdit = session.query(MenuItem).filter_by(id = menu_id).one()
-	if request.method == 'POST':
+    itemToEdit = session.query(MenuItem).filter_by(id = menu_id).one()
+    if request.method == 'POST':
 		if request.form['newname']:
 			itemToEdit.name = request.form['newname']
 		if request.form['description']:
-            editedItem.description = request.form['description']
-        if request.form['price']:
-            editedItem.price = request.form['price']
-        if request.form['course']:
-            editedItem.course = request.form['course']
+			itemToEdit.description = request.form['description']
+		if request.form['price']:
+			itemToEdit.price = request.form['price']
+		if request.form['course']:
+			itemToEdit.course = request.form['course']
 		session.add(itemToEdit)
 		session.commit()
 		flash("Menu item has been successfully edited!")
-		return redirect(url_for(restaurantMenu, restaurant_id = restaurant_id))
+		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
     else:
     	return render_template('editmenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = itemToEdit)
 
@@ -81,15 +79,15 @@ def deleteMenuItem(restaurant_id, menu_id):
 		session.delete(itemToDelete)
 		session.commit()
 		flash("Menu item has been successfully deleted!")
-		return redirect(url_for(restaurantMenu, restaurant_id = restaurant_id))
-    else:
-    	return render_template('deletemenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = itemToDelete)
+		return redirect(url_for('restaurantMenu', restaurant_id = restaurant_id))
+	else:
+		return render_template('deletemenuitem.html', restaurant_id = restaurant_id, menu_id = menu_id, item = itemToDelete)
 
 
 
 
 
 if __name__ == '__main__':
-	app.secret_key = super_secret_key
-    app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+	app.secret_key = 'super_secret_key'
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
